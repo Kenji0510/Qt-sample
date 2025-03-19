f4144c9c30149b7580500a9b6fe3b6af1d72b445
Kenji0510 <yukkurireimupikacyuu@gmail.com>
2025-03-19 18:12:39 +0900

    2025/3/19

--- src/VertexShaderCode.glsl
+++ src/VertexShaderCode.glsl
@@ -1,12 +1,12 @@
 #version 400
 
-layout(location = 0) in vec2 position;
+layout(location = 0) in vec3 position;
 layout(location = 1) in vec3 vertexColor;
 
 out vec3 theColor;
 
 void main()
 {
-    gl_Position = vec4(position, 0.0, 1.0);
+    gl_Position = vec4(position, 1.0);
     theColor = vertexColor;
 }
--- src/openglsample.cpp
+++ src/openglsample.cpp
@@ -36,6 +36,7 @@ void OpenGLWindow::initializeGL()
     // glClearColor(0.0f, 0.5f, 0.8f, 1.0f);\
 
     glewInit();
+    glEnable(GL_DEPTH_TEST);
     sendDataToOpenGL();
     installShaders();
 }
@@ -69,29 +70,34 @@ void OpenGLWindow::installShaders()
 
 void OpenGLWindow::sendDataToOpenGL()
 {
+    const float RED_TRIANGLE_Z = 0.5f;
+    const float BLUE_TRIANGLE_Z = -0.5f;
     GLfloat verts[] =
     {
-        +0.0f, +0.0f,
+        -1.0f, -1.0f, RED_TRIANGLE_Z,
         +1.0f, +0.0f, +0.0f,
-        +1.0f, +1.0f,
-        +0.0f, +1.0f, +0.0f,
-        -1.0f, +1.0f,
+        +0.0f, +1.0f, RED_TRIANGLE_Z,
+        +1.0f, +0.0f, +0.0f,
+        +1.0f, -1.0f, RED_TRIANGLE_Z,
+        +1.0f, +0.0f, +0.0f,
+
+        -1.0f, +1.0f, BLUE_TRIANGLE_Z,
         +0.0f, +0.0f, +1.0f,
-        -1.0f, -1.0f,
+        +0.0f, -1.0f, BLUE_TRIANGLE_Z,
         +0.0f, +0.0f, +1.0f,
-        +1.0f, -1.0f,
-        +0.0f, +1.0f, +0.0f,
+        +1.0f, +1.0f, BLUE_TRIANGLE_Z,
+        +0.0f, +0.0f, +1.0f,
     };
     GLuint vertexBufferID;
     glGenBuffers(1, &vertexBufferID);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 5, 0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0);
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3));
     
-    GLushort indices[] = {0,1,2, 0,3,4};
+    GLushort indices[] = { 0,1,2, 3,4,5,};
     GLuint indexBufferID;
     glGenBuffers(1, &indexBufferID);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
@@ -101,6 +107,7 @@ void OpenGLWindow::sendDataToOpenGL()
 void OpenGLWindow::paintGL()
 {
     // glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_DEPTH_BUFFER_BIT);
     glViewport(0, 0, width(), height());
     // glDrawArrays(GL_TRIANGLES, 0, 6);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
